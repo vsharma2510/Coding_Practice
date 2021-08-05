b4ec8389cf752f4bb0e33d089b6188200998bdf1
Vivek Sharma <vsharma2510@gmail.com>
2021-08-06 00:49:47 +0200

    Macro to delete file from C++ code

--- Cpp/delete_file.cpp
+++ Cpp/delete_file.cpp
@@ -1,20 +1,24 @@
 //Testing code to delete text file within C++ code
 #include <fstream>
-#include <boost/filesystem.hpp>
+#include <iostream>
+//#include <boost/filesystem.hpp>
 #include "TString.h"
-
-
+#include <stdio.h>
 
 using namespace std;
-
+//using namespace boost;
 int main(){
   
-  TString file_name = Form("test_text_file.txt");
+  //filesystem::path file_name;
+  TString file_name = Form("test_file.txt");
+  //ofstream test_file;
+  std::remove(file_name.Data());
   ofstream test_file;
   test_file.open(file_name,std::ofstream::app);
   test_file<<"test"<<"\n";
   test_file.close();
-
+ 
+  cout<<"Done"<<endl;
   return 0;
 }
   

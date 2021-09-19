50463e11e3e10240c9e9eeebe05f983296c6b7c3
vsharma2510 <vsharma2510@gmail.com>
2021-09-18 23:35:24 -0400

    Update Collatz.py

--- Python/Collatz.py
+++ Python/Collatz.py
@@ -1,1 +1,26 @@
+import sys
+def Collatz(number):
+    if(number==1):
+        print('Entering 1 is not allowed')
+        sys.exit()
+    if(number%2):
+        return (3*number+1)
+    else:
+        return (number//2)
+
 print('Enter number')
+number_string = input()
+try:
+    number = int(number_string)
+except ValueError:
+    print('!!!!! Please enter an integer !!!!!')
+    print('Executing the program with default value 3')
+    number=3
+while(number!=1):
+    temp=Collatz(number)
+    print(str(temp)+'\n')
+    number=temp
+if(number==1):
+    print('Sequence finished')
+else:
+    print('Something went wrong!')

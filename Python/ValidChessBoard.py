199ea6175e82d96425d9028294ee93aaaf2c328e
vsharma2510 <vsharma2510@gmail.com>
2021-10-21 14:29:18 -0400

    Adding funcionality to check position validity

--- Python/ValidChessBoard.py
+++ Python/ValidChessBoard.py
@@ -7,12 +7,21 @@ def isValidChessBoard(board):
     num_bbishop,num_wbishop,num_brook,num_wrook=0,0,0,0
 
     for k,v in board.items():
+        position=[]
+        position[:0]=k
+        #print(position[0])
+        #print(position[1])
+        if len(position)!=2:
+            print('!!! INVALID POSITION COORDINATES ' + k)
+        if (int(position[0])>8 or int(position[0])<1) or (position[1]>'h' or position[1]<'a'):
+            print('INVALID PIECE POSITION '+k)
+        
 
 
     return num_bking
 
 
 
-chess_board={'1h':'bking','6c':'wqueen','2g':'bbishop','5h':'bqueen','3e':'wking'}
+chess_board={'9h':'bking','6c':'wqueen','2g':'bbishop','5h':'bqueen','3e':'wking'}
 
 isValidChessBoard(chess_board)
